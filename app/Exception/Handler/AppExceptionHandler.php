a53da0c0b20be0bd7f01916a0001bd88085df455
Luis Felipe <felipe.drs01@gmail.com>
2024-02-11 16:38:17 -0300

    feat: add custom exception to app exception handler

--- app/Exception/Handler/AppExceptionHandler.php
+++ app/Exception/Handler/AppExceptionHandler.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 namespace App\Exception\Handler;
 
 use App\Constants\HttpStatus;
+use App\Exception\ResourceNotFoundException;
 use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\ExceptionHandler\ExceptionHandler;
 use Hyperf\HttpMessage\Stream\SwooleStream;
@@ -27,18 +28,25 @@ class AppExceptionHandler extends ExceptionHandler
 
     public function handle(Throwable $throwable, ResponseInterface $response)
     {
-        if ($throwable instanceof InvalidArgumentException) {
-            $data = [
-                'error' => $throwable->getMessage(),
-            ];
+        $data = [
+            'error' => $throwable->getMessage(),
+        ];
 
+        if ($throwable instanceof InvalidArgumentException) {
             return $response->withHeader('Content-Type', 'application/json')
                 ->withStatus(HttpStatus::UNPROCESSABLE_ENTITY)
                 ->withBody(new SwooleStream(json_encode($data)));
         }
 
+        if ($throwable instanceof ResourceNotFoundException) {
+            return $response->withHeader('Content-Type', 'application/json')
+                ->withStatus(HttpStatus::NOT_FOUND)
+                ->withBody(new SwooleStream(json_encode($data)));
+        }
+
         $this->logger->error(sprintf('%s[%s] in %s', $throwable->getMessage(), $throwable->getLine(), $throwable->getFile()));
         $this->logger->error($throwable->getTraceAsString());
+
         return $response->withHeader('Server', 'Hyperf')
             ->withStatus(HttpStatus::INTERNAL_SERVER_ERROR)
             ->withBody(new SwooleStream(json_encode(['error' => 'Ocorreu um problema inesperado.'])));
