5ab3e7d368c0f2abc8016b615bfe28550ce82866
Luis Felipe <felipe.drs01@gmail.com>
2024-02-12 03:56:54 -0300

    feat: add openAi annotations to user and transaction controllers

--- app/Controller/TransactionController.php
+++ app/Controller/TransactionController.php
@@ -10,6 +10,7 @@ use App\Application\Usecase\Transaction\MakeTransactionUseCase;
 use App\Infra\Validation\MakeTransactionValidator;
 use Hyperf\HttpServer\Contract\ResponseInterface;
 use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
+use OpenApi\Annotations as OA;
 
 class TransactionController
 {
@@ -18,6 +19,34 @@ class TransactionController
         private readonly TransactionMapper $transactionMapper
     ){}
 
+    /**
+     * @OA\Post(
+     *     path="/transactions",
+     *     summary="Realiza uma nova transação",
+     *     description="Realiza uma nova transação com base nos parâmetros fornecidos",
+     *     @OA\RequestBody(
+     *         description="Objeto de transação",
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(property="payer", type="string", format="uuid", description="ID do pagador"),
+     *                 @OA\Property(property="payee", type="string", format="uuid", description="ID do beneficiário"),
+     *                 @OA\Property(property="value", type="string", format="decimal", description="Valor da transação (por exemplo: '10.00')")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Sucesso",
+     *         @OA\Schema(ref="#/components/schemas/TransactionResultObject")
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Recurso não encontrado"
+     *     )
+     * )
+     */
     public function makeTransaction(MakeTransactionValidator $request, ResponseInterface $response): PsrResponseInterface
     {
         $request = $request->validated();
--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -13,7 +13,15 @@ use App\Infra\Validation\CreateUserValidator;
 use App\Infra\Validation\WalletOperationValidator;
 use Hyperf\HttpServer\Contract\ResponseInterface;
 use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *     title="Transactions Api",
+ *     version="0.1"
+ * ),
+ * @OA\Server(url="http://localhost:9501")
+ */
 class UserController
 {
     public function __construct(
@@ -23,6 +31,37 @@ class UserController
         private readonly UserMapper $userMapper
     ){}
 
+    /**
+     * @OA\Post(
+     *     path="/users",
+     *     summary="Cria um novo usuário",
+     *     description="Cria um novo usuário com base nos parâmetros fornecidos",
+     *     @OA\RequestBody(
+     *         description="Objeto de busca do lado do cliente",
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(property="userType", type="string", description="Tipo de usuário (por exemplo: 'common')"),
+     *                 @OA\Property(property="name", type="string", description="Nome do usuário"),
+     *                 @OA\Property(property="email", type="string", format="email", description="Endereço de e-mail do usuário"),
+     *                 @OA\Property(property="password", type="string", description="Senha do usuário"),
+     *                 @OA\Property(property="documentType", type="string", description="Tipo de documento (por exemplo: 'cnpj')"),
+     *                 @OA\Property(property="document", type="string", description="Número do documento (por exemplo: '32618735000170')")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Sucesso",
+     *         @OA\Schema(ref="#/components/schemas/SearchResultObject")
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Recurso não encontrado"
+     *     )
+     * )
+     */
     public function createUser(CreateUserValidator $request, ResponseInterface $response): PsrResponseInterface
     {
         $request = $request->validated();
@@ -30,6 +69,32 @@ class UserController
         return $response->json($userAccount->export())->withStatus(HttpStatus::CREATED);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/users/{id}/deposit",
+     *     summary="Realiza uma nova transação",
+     *     description="Realiza uma nova transação com base nos parâmetros fornecidos",
+     *     @OA\RequestBody(
+     *         description="Objeto de valor da transação",
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(property="value", type="string", format="decimal", description="Valor da transação (por exemplo: '500.00')")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Sucesso",
+     *         @OA\Schema(ref="#/components/schemas/TransactionResultObject")
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Recurso não encontrado"
+     *     )
+     * )
+     */
     public function handleDeposit(WalletOperationValidator $request, string $id, ResponseInterface $response): PsrResponseInterface
     {
         $request = $request->validated();
@@ -37,6 +102,15 @@ class UserController
         return $response->json($wallet->export())->withStatus(HttpStatus::OK);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/users/{id}",
+     *     @OA\Response(
+     *         response="200",
+     *         description="The data"
+     *     )
+     * )
+     */
     public function findUserById(string $id, ResponseInterface $response): PsrResponseInterface
     {
         $userAccount = $this->findUserAccountUseCase->execute($id);
